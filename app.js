f3c1f71f06a9efc868f6550b7d4d068ce62ed8e8
Amar <imsirovicamar000@gmail.com>
2021-11-04 15:14:40 -0700

    new project for fast food

---
+++ app.js
@@ -0,0 +1,48 @@
+
+// Menu
+const menuSpan = document.querySelectorAll('.menu-span');
+menuSpan.forEach(el =>{
+  if(el.textContent == ''){
+    el.style.backgroundColor = "white";
+  }
+})
+
+// Burger Slider
+const swiper = new Swiper('.burger-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 16,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next-unique',
+      prevEl: '.swiper-button-prev-unique',
+    },
+
+    breakpoints: {
+      600: {
+          slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      
+      1200:{
+        slidesPerView: 4,
+      }
+  }
+  });
+
+  //  Testimonial Slider
+const swiper2 = new Swiper('.testimonial-slider', {
+  // Optional parameters
+  slidesPerView: 1,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  
+
+});
