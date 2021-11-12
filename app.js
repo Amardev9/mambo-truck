24de42cab01f27d1fc17f121024e3c6ef1698ffc
Amar <imsirovicamar000@gmail.com>
2021-11-12 08:09:55 -0800

    added fade animation on hero page

--- app.js
+++ app.js
@@ -1,14 +1,65 @@
+const body = document.getElementById('body');
 
-// Menu
-const menuSpan = document.querySelectorAll('.menu-span');
-menuSpan.forEach(el =>{
-  if(el.textContent == ''){
-    el.style.backgroundColor = "white";
-  }
-})
+document.addEventListener('DOMContentLoaded', () => {
+  // animation
+  AOS.init({
+    once: true,
+    easing: 'ease-in-sine',
+    duration: 600,
+  });
 
-// Burger Slider
-const swiper = new Swiper('.burger-slider', {
+  // Spinner
+
+  const loadingOverlay = document.querySelector('.loading-overlay');
+  setTimeout(() => {
+    body.classList.remove('overflow-h');
+    loadingOverlay.classList.add('hide-loading');
+  }, 2000);
+
+  // Nav
+  const toggler = document.querySelector('.nav-toggler');
+  const overlay = document.querySelector('#overlay');
+  const close = document.querySelector('.close');
+
+  toggler.addEventListener('click',()=>{
+    overlay.classList.add('nav-open');
+    
+  })
+
+  close.addEventListener('click',()=>{
+    overlay.classList.remove('nav-open');
+  })
+  
+  const mobileNavLink = document.querySelectorAll('.m');
+  mobileNavLink.forEach(element => {
+    element.addEventListener('click', ()=>{
+      overlay.classList.remove('nav-open');
+    })
+  });
+
+  const goBackBtn = document.querySelector('.go-back-btn');
+  window.addEventListener('scroll',()=>{
+    if(window.scrollY > 500){
+      goBackBtn.classList.add('show');
+    }
+
+    if(window.scrollY < 500){
+      if(goBackBtn.classList.contains('show')){
+        goBackBtn.classList.remove('show');
+      }
+    }
+  })
+
+  // Menu
+  const menuSpan = document.querySelectorAll('.menu-span');
+  menuSpan.forEach(el =>{
+    if(el.textContent == ''){
+      el.style.backgroundColor = "white";
+    }
+  })
+
+  // Burger Slider
+  const swiper = new Swiper('.burger-slider', {
     // Optional parameters
     loop: true,
     slidesPerView: 1,
@@ -30,20 +81,33 @@ const swiper = new Swiper('.burger-slider', {
       1200:{
         slidesPerView: 4,
       }
-  }
+    }
   });
 
   //  Testimonial Slider
-const swiper2 = new Swiper('.testimonial-slider', {
+  const swiper2 = new Swiper('.testimonial-slider', {
   // Optional parameters
   slidesPerView: 1,
   spaceBetween: 16,
+  effect: 'fade',
+  autoplay: {
+    delay: 5000,
+  },
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-  
 
-});
+  });
+})
+
+
+
+
+
+
+
+
+
